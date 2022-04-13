cdb1d5b54ffbd4737078fb9e96b6c8c0b48383d7
Purushottam <Purushottam@LAPTOP-V9S0IH9G>
2022-04-13 14:36:24 +0530

    Third commit

---
+++ Day04_packages_encapsulation_abstraction/src/com/tns/pack3/SBI.java
@@ -0,0 +1,15 @@
+package com.tns.pack3;
+
+public class SBI {
+	
+	public int balance;
+	public int getBalance()
+	{
+		return balance;
+	}
+	public void setBalance(int bal)
+	{
+		balance=bal;
+	}
+
+}
